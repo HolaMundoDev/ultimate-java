d45f1088e5846178ed9482ad00c4343305f65136
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-03 18:03:11 -0600

    Sección 02: Tipos básicos - Clase 06: Multiples dimensiones.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] numeros = new int[5];
-        int numeros[] = {0, 1, 2, 3, 4};
-        numeros[0] = 5;
-        numeros[4] = 7;
-//        numeros[7] = 10; // Error
-        System.out.println(Arrays.toString(numeros));
-        System.out.println(numeros.length);
-        Arrays.sort(numeros);
-        System.out.println(Arrays.toString(numeros));
+        int[][][] numeros = new int[3][2][2];
+        numeros[0][0][0] = 5;
+        System.out.println(Arrays.deepToString(numeros));
     }
 }
