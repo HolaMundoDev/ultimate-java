e88b69bf83a9def76ea1d83897a3cffa9d928793
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-03 18:29:23 -0600

    Sección 02: Tipos básicos - Clase 08: Comentarios.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        // Esto es un comentario de una línea.
+
+
+        // Forma 1
+        /* Escribir todos
+        * los comentarios
+        * que queramos
+        */
+
+        // Forma 2
+        /**
+         *  aoeaoe
+         *  aoe
+         *
+         *  oeu
+         */
+
         final String empresa = "Hola Mundo Limited";
-        empresa = "Chanchito Feliz";
+        // empresa = "Chanchito Feliz"; // Error
         System.out.println(empresa);
     }
 }
