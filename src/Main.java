c017b494b7dc151317bd57871d4f48b9283b9dc2
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-03 18:15:51 -0600

    Sección 02: Tipos básicos - Clase 07: Constantes.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][][] numeros = new int[3][2][2];
-        numeros[0][0][0] = 5;
-        System.out.println(Arrays.deepToString(numeros));
+        final String empresa = "Hola Mundo Limited";
+        empresa = "Chanchito Feliz";
+        System.out.println(empresa);
     }
 }
