e5ac23a8ce25cee3d5a4a98571e8d2758f07513e
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-12 17:06:08 -0600

    Sección 02: Tipos básicos - Clase 15. Ejercicio Calculadora.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /**
+         * Crear una calculadora que reciba
+         * 2 números y luego imprima el resultado de la
+         * suma de estos dos números
+         */
+        System.out.println("Esta es una aplicación de calculadora.");
+        System.out.println("Ingrese el primer número: ");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingresa tu edad:");
-        Byte valor = scanner.nextByte();
-        System.out.println("Tu edad es:");
-        System.out.println(valor);
+        int valorA = scanner.nextInt();
+        System.out.println("Ingrese el segundo número: ");
+        int valorB = scanner.nextInt();
+        System.out.println("El resultado de la suma es: ");
+        System.out.println(valorA + valorB);
     }
 }
