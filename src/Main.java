fe7998d7a50efd53a0da0dd48b209d04319f2aed
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-12 17:18:01 -0600

    Sección 02: Tipos básicos - Clase 17. var.

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-//        int a = 15;
-//        int b = a;
-//
-//        b = 33;
-//        System.out.println(a);
-
-//        Point point1 = new Point();
-//        Point point2 = point1;
-//
-//        point2.x = 15;
-//        System.out.println(point1);
-
-        String a = new String("Hola Mundo");
-        String b = a;
-        String c = a.toUpperCase();
-        System.out.println(c);
+        var x = 45;
     }
 }
