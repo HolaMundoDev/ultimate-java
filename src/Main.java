bff288f229c085117ab54267542854b51041d58c
Gabriel Hernandez <onedrakodev@gmail.com>
2025-01-31 19:20:00 -0600

    Sección 03: Control de flujo - Clase 02 . Operadores lógicos.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 10;
+        boolean a = true;
+        boolean b = false;
 
-        System.out.println(a == b);
-        System.out.println(a != b);
+        boolean c = a || b; // true
+        boolean d = a && b; // false
 
-        System.out.println(a > b);
-        System.out.println(a >= b);
+        System.out.println(c);
+        System.out.println(d);
 
-        System.out.println(a < b);
-        System.out.println(a <= b);
+        boolean x = !a; // false
+        System.out.println(x);
+
+        boolean isAdult = true;
+        boolean isActive = false;
+        boolean isPremium = true;
+        boolean isON = true;
+
+        String coupon = "chanchito_feliz";
+
+        boolean promo = coupon.equals("ultimate_java") ||  coupon.equals("chanchito_feliz");
+
+        boolean hasFunds = true;
+
+        boolean canPurchase = isAdult && isActive && isPremium && isON && promo;
     }
 }
