d545d3a15e9a6ea86bc8e6d2c09f2399ca7f5c22
Gabriel Hernandez <onedrakodev@gmail.com>
2025-03-27 21:22:21 -0600

    Sección 03: Control de flujo - Clase 13. break y continue.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int i = 2;
-        do {
-            System.out.println(i);
-            i++;
-        } while (i < 5);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (true) {
+            System.out.println("Ingresa un comando: ");
+            input = scanner.next().toLowerCase();
+
+            if (input.equals("salir")){
+                break;
+            }
+
+            if (input.equals("continuar")){
+                continue;
+            }
+
+            System.out.println("Esta línea no se debería ejecutar");
+
+        }
+        System.out.println("Termino el loop");
+
     }
 }
