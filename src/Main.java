60e5e52514e265a277a2d4d5d6e5eb32fcb01011
Gabriel Hernandez <onedrakodev@gmail.com>
2025-03-27 20:44:11 -0600

    Sección 03: Control de flujo - Clase 12. do while.

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-
-        while (!input.equals("salir")) {
-            System.out.println("Ingresa comando: ");
-            input = scanner.next().toLowerCase();
-            System.out.println(input);
-        }
-
-        System.out.println("Termino el loop.");
+        int i = 2;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i < 5);
     }
 }
