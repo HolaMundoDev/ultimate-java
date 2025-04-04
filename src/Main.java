fd4859fe35f5d9e3758a572898bbae4779e97d19
Gabriel Hernandez <onedrakodev@gmail.com>
2025-04-03 20:59:51 -0600

    Sección 04: Métodos - Clase 02. Parámetros y argumentos.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        miMetodo();
-        miMetodo();
-        miMetodo();
-        otroMetodo();
-        otroMetodo();
+        miMetodo("Nicolas", 25);
+        miMetodo("Chanchito feliz", 30);
     }
 
-    static void miMetodo(){
-        System.out.println("Hola mundo");
-    }
-
-    static void otroMetodo(){
-        System.out.println("Chanchito Feliz");
+    static void miMetodo(String nombre, int edad){
+        System.out.println("Hola " + nombre + ", tu edad es: " + edad);
     }
 }
