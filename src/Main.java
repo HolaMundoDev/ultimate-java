0ff9938e26b7c7f9e835051a291d6351f51ff49b
Gabriel Hernandez <onedrakodev@gmail.com>
2025-04-03 20:51:46 -0600

    Sección 04: Métodos - Clase 01. Métodos.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
+        miMetodo();
+        miMetodo();
+        miMetodo();
+        otroMetodo();
+        otroMetodo();
+    }
 
-        terminal: while(true){
-            System.out.println("Ingresa una opción: ");
-            System.out.println("1. Calculadora");
-            System.out.println("2. Hackear la nasa");
-            System.out.println("3. Salir");
+    static void miMetodo(){
+        System.out.println("Hola mundo");
+    }
 
-            input = scanner.next().toLowerCase();
-
-            switch (input){
-                case "1":
-                    System.out.println("Ingrese un número: ");
-                    int a = scanner.nextInt();
-                    System.out.println("Ingrese segundo número: ");
-                    int b = scanner.nextInt();
-                    System.out.println("El rusultado es: ");
-                    System.out.println(a + b);
-                    break;
-                case "2":
-                    System.out.println("Hackeando la nasa 😎");
-                    System.out.println("#");
-                    System.out.println("##");
-                    System.out.println("###");
-                    System.out.println("####");
-                    System.out.println("Nasa hackeada, eres hackerman!");
-                    break;
-                case "3":
-                    System.out.println("Chao mundo");
-                    break terminal;
-            }
-        }
+    static void otroMetodo(){
+        System.out.println("Chanchito Feliz");
     }
 }
