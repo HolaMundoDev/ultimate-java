d990e45e6ca10641a5018c8148486a170cf1243a
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-06 13:12:00 -0600

    Sección 04: Métodos - Clase 04. Overloading.

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int resultado = suma(12, 25);
+        int resultado = suma(12, 25, 45);
+        int resultado2 = suma(1,2);
         System.out.println(resultado);
+
+        String texto = suma();
     }
     static int suma(int a, int b){
         return a + b;
+    }
+
+    static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
+    static  String suma(){
+        return "Hola mundo";
     }
 }
