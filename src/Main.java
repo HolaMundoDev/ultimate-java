f21d42abb9e289e312272caa1e9b84537befe50c
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-10 18:36:58 -0600

    Sección 05: Orientación a objetos - Clase 02. Propiedades.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         User user = new User();
+        user.name = "Nicolas";
+
+        User user2 = new User();
+        user2.name = "Felipe";
+
+        System.out.println(user.name);
+        System.out.println(user2.name);
     }
 }
