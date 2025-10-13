ac6db16008615ca530a40f584c54114bf536b556
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-13 14:31:49 -0600

    Sección 05: Orientación a objetos - Clase 05. Método constructor.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        User user = new User();
-        user.name = "Nicolas";
-
-        User user2 = new User();
-        user2.name = "Felipe";
+        User user = new User("Nicolas");
+        User user2 = new User("Felipe");
         
         user.saludar();
         user2.saludar();
--- src/User.java
+++ src/User.java
@@ -2,6 +2,9 @@ class User {
     double id = Math.random();
     String name;
 
+    User(String name){
+        this.name = name;
+    }
     public void saludar(){
         System.out.println("Hola mundo! soy " + this.name);
     }
