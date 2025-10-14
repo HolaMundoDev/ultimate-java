f93821dffc635a9cc92276c619aaf05cd1173508
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-13 19:58:47 -0600

    Sección 05: Orientación a objetos - Clase 14. Creando paquetes.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
+import Usuarios.User;
+
 public class Main {
     public static void main(String[] args) {
-        User user = new User("Nicolas");
-
-        user.login();
-
-        String pass = user.getPassword();
-        System.out.println(pass);
-        user.setPassword("13134545");
-        System.out.println(user.getPassword());
-
-        User user2 = new User("Hola mundo", "chanchito@feliz.com");
+        User u = new User("Chanchito feliz");
     }
 }
