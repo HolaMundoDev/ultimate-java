191a21a6f2cbdc700491f5351e705c14f6b2e519
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-17 14:14:16 -0600

    Sección 07: Herencia. - Clase 2. Herencia.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,10 @@
+import recursos.Categorias;
+import recursos.Productos;
+
 public class Main {
     public static void main(String[] args) {
-        /**
-         * Crear la clase VideoGame
-         * con las propiedades
-         * name String
-         * price double
-         * category String
-         *
-         * fetters y setters para toda las propiedades
-         */
-
-//        VideoGame vg = new VideoGame("God of war", 5);
-        VideoGame vg = new VideoGame("God of war", 25);
-
-        vg.setName("God of war");
-//        vg.setCategory("Chanchito feliz");
-        vg.setCategory("action");
-        System.out.println(vg.getCategory());
-
-        System.out.println(vg.getName());
-        System.out.println(vg.getPrice());
+        Categorias c = new Categorias();
+        Productos p = new Productos();
+//        p.getName();
     }
 }
