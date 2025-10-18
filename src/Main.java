13dc6d8fb672a6f0f8fc8e76d9df4c971f6f5e91
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-17 19:58:58 -0600

    Sección 08: Interfaces. - Clase 6. Implementando multiples interfaces.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import recursos.Garzon;
-import recursos.Restaurante;
+import recursos.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,7 +7,9 @@ public class Main {
         Garzon g1 = new Garzon();
         Garzon g2 = new Garzon();
         Garzon g3 = new Garzon();
+        Cocinero c = new Cocinero();
 
-        r.llevarCervezas(new Garzon[]{g1,g2,g3});
+        r.llevarCervezas(new Atendible[]{g1, g2, g3, c});
+        Cocinable cs = new Cocinero();
     }
 }
