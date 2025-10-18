51282aef0b23f25ea9626b17399961e3f8daeac4
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-17 19:50:08 -0600

    Sección 08: Interfaces. - Clase 5. Creando interface.

--- src/recursos/Garzon.java
+++ src/recursos/Garzon.java
@@ -1,7 +1,16 @@
 package recursos;
 
-public class Garzon {
+public class Garzon extends Base implements Atendible {
+    public Garzon() {
+        super("Chanchito feliz");
+    }
+
     public void atenderMesa(){
         System.out.println("Atiende mesa");
+    }
+
+    @Override
+    public void guardar() {
+
     }
 }
