6eb4dfaeb53bfcc1beffa88c6a83491cf6c87c73
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-17 19:42:13 -0600

    Sección 08: Interfaces. - Clase 4. Inyección de dependencias.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import recursos.Garzon;
+import recursos.Restaurante;
+
 public class Main {
     public static void main(String[] args) {
+        Restaurante r = new Restaurante();
+
+        Garzon g1 = new Garzon();
+        Garzon g2 = new Garzon();
+        Garzon g3 = new Garzon();
+
+        r.llevarCervezas(new Garzon[]{g1,g2,g3});
     }
 }
