9b2c35a4580019e2a0efc7ff3e037ce8be89a5a6
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-20 15:50:35 -0600

    Sección 09: Genéricos. - Clase 12. Wildcards.

--- src/genericos/Utils.java
+++ src/genericos/Utils.java
@@ -4,4 +4,18 @@ public class Utils {
     public static <T extends Number> T max(T a, T b){
         return a.doubleValue() > b.doubleValue() ? a : b;
     }
+
+//    public static <T extends Garzon> Lista<T> sorted(Lista<T> lista){
+//        return lista;
+//    }
+
+//    public static void sorted(Lista<? extends Garzon> lista){
+//        lista.get(0);
+//        lista.add(new Garzon(3)); // Error
+//    }
+
+    public static void sorted(Lista<? super Garzon> lista){
+//        Garzon x = lista.get(0); // Error
+        lista.add(new Garzon(3));
+    }
 }
