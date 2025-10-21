d74a9f0c21e8da5514460f20337e5421abe7cf34
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-21 17:37:37 -0600

    Sección 11: Colecciones. - Clase 4. Iterable.

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,16 @@ import colecciones.MiColeccion;
 public class Main {
     public static void main(String[] args) {
         MiColeccion.run();
+
+        var lista = new Lista<>();
+
+//        while(iterator.hasNext()){
+//            var elemento = iterator.next();
+//            System.out.println(elemento);
+//        }
+
+        for (Object elemento : lista) {
+            System.out.println(elemento);
+        }
     }
 }
---
+++ src/colecciones/Lista.java
@@ -0,0 +1,23 @@
+package colecciones;
+
+import java.util.Iterator;
+
+public class Lista<T> implements Iterable<T> {
+    private T[] data = (T[]) new Object[50];
+    private int length = 0;
+
+    public Lista<T> add(T t){
+        data[length++] = t;
+
+        return this;
+    }
+
+    public T get(int index){
+        return data[index];
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+}
