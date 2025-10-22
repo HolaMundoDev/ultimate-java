0eb4b24788c5d0624865f2468c7161a8d76af424
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-22 15:06:14 -0600

    Sección 12: Expresiones Lambda. - Clase 5. Métodos po referencia.

--- src/lambda/Demo.java
+++ src/lambda/Demo.java
@@ -2,21 +2,26 @@ package lambda;
 
 public class Demo {
     private static String texto = "Imprimiendo de lambda: ";
-//    private String texto = "Imprimiendo de lambda: ";
     private static Notificable lambda = message ->
         System.out.println(texto + message);
 
-    public static void run(){
-//        var texto = "Imprimiendo de lambda: ";
-//        Notificable lambda = message ->
-//            System.out.println(texto + message);
-//        ;
+    public Demo(String message){}
+    public static void metodo(String message){
 
-//        Notificable lambda = message -> {
-//                var texto = "Imprimiendo de lambda: ";
-//                System.out.println(texto + message);
-//        };
+    }
+    public void metodoInstancia(String message){}
+
+    public static void run(){
         push(lambda);
+
+        push(message -> metodo(message));
+
+        push(Demo::metodo);
+        push(System.out::println);
+        
+        Demo demo = new Demo("mi mensaje");
+        push(demo::metodoInstancia);
+        push(Demo::new);
     }
 
     public static void push(Notificable n){
