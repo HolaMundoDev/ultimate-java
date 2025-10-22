381eca6934b290bfc895880d4a23b36bd7b12f66
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-21 18:02:49 -0600

    Sección 11: Colecciones. - Clase 6. List.

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,37 @@
 import colecciones.Lista;
 import colecciones.MiColeccion;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 
 public class Main {
     public static void main(String[] args) {
-        MiColeccion.run();
+//        MiColeccion.run();
+//
+//        var lista = new Lista<>();
+//        lista.add(1);
+//        lista.add(2);
+//
+//
+//        for (Object elemento : lista) {
+//            System.out.println("Iterando... " + elemento);
+//        }
 
-        var lista = new Lista<>();
-        lista.add(1);
-        lista.add(2);
+        List<Integer> lista = new ArrayList<>();
+        Collections.addAll(lista, 1,2,3,4,50);
 
+        var elemento = lista.get(2);
+        lista.set(2, 50);
 
-        for (Object elemento : lista) {
-            System.out.println("Iterando... " + elemento);
-        }
+        System.out.println(lista.indexOf(50));
+        System.out.println(lista.indexOf(49));
+
+        System.out.println(lista.lastIndexOf(50));
+
+        System.out.println(lista.subList(0, 3));
+        System.out.println(lista);
     }
 }
