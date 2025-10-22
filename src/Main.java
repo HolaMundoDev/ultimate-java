0534923074edea1b2b13f1a541e9b0c909984355
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-22 12:38:47 -0600

    Sección 12: Expresiones Lambda. - Clase 1. Introducción.

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,7 @@
-
-import colecciones.*;
-
-import java.util.*;
-
+import lambda.Demo;
 
 public class Main {
     public static void main(String[] args) {
-//        MiColeccion.run();
-//
-//        var lista = new Lista<>();
-//        lista.add(1);
-//        lista.add(2);
-//
-//
-//        for (Object elemento : lista) {
-//            System.out.println("Iterando... " + elemento);
-//        }
-
-//        List<Integer> lista = new ArrayList<>();
-//        Collections.addAll(lista, 1,2,3,4,50);
-//
-//        var elemento = lista.get(2);
-//        lista.set(2, 50);
-//
-//        System.out.println(lista.indexOf(50));
-//        System.out.println(lista.indexOf(49));
-//
-//        System.out.println(lista.lastIndexOf(50));
-//
-//        System.out.println(lista.subList(0, 3));
-//        System.out.println(lista);
-
-//        List<EjemploComparable> lista = new ArrayList<EjemploComparable>();
-//        EjemploComparable e2 = new EjemploComparable("Felipe", 15);
-//        EjemploComparable e1 = new EjemploComparable("Chanchito Feliz", 22);
-//
-//        lista.add(e1);
-//        lista.add(e2);
-//
-//        Collections.sort(lista, new EdadComparator());
-//        System.out.println(lista);
-//        EjemploSet.run();
-//        DemoDeque.run();
-        EjemploMap.map();
+        Demo.run();
     }
 }
