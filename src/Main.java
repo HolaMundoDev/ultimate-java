bf5eee692a45f7e9524ea28dc13117b581b4490f
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-22 15:06:55 -0600

    Sección 12: Expresiones Lambda. - Clase 7. Consumer.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import lambda.Demo;
 
 public class Main {
     public static void main(String[] args) {
-        Demo.run();
+        Demo.incluidas();
     }
 }
--- src/lambda/Demo.java
+++ src/lambda/Demo.java
@@ -1,5 +1,7 @@
 package lambda;
 
+import java.util.List;
+
 public class Demo {
     private static String texto = "Imprimiendo de lambda: ";
     private static Notificable lambda = message ->
@@ -18,13 +20,25 @@ public class Demo {
 
         push(Demo::metodo);
         push(System.out::println);
-        
+
         Demo demo = new Demo("mi mensaje");
         push(demo::metodoInstancia);
         push(Demo::new);
     }
 
+    public static void incluidas(){
+        List<String> l = List.of("Hola", "Mundo");
+
+//        l.forEach(el -> System.out.println(el));
+//        l.forEach(System.out::println);
+        l.forEach(Demo::sendPush);
+    }
+
     public static void push(Notificable n){
         n.push("Chanchito feliz");
+    }
+
+    public static void sendPush(String id){
+        System.out.println("Enviando notificación push " + id);
     }
 }
