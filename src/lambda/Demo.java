916d30c03f5f8b402c4efd9084381b8af6405d9c
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-22 12:56:58 -0600

    Sección 12: Expresiones Lambda. - Clase 2. Clases anónimas.

--- src/lambda/Demo.java
+++ src/lambda/Demo.java
@@ -2,7 +2,12 @@ package lambda;
 
 public class Demo {
     public static void run(){
-        push(new Notification());
+        push(new Notificable() {
+            @Override
+            public void push(String message) {
+                System.out.println("Enviando desde clase anónima: " + message);
+            }
+        });
     }
 
     public static void push(Notificable n){
