9c12d7091ba34b3b4d037b7f55944c6cc59f470b
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-22 19:43:06 -0600

    Sección 13: Streams. - Clase 3. map.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
-import streams.Creacion;
+import streams.DemoMap;
 
 public class Main {
     public static void main(String[] args) {
-        Creacion.creacion();
+        DemoMap.run();
     }
 }
