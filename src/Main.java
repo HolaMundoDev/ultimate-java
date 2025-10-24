be0ac4f36a81852a07801ca2d776fc8429872934
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-23 18:34:57 -0600

    Sección 13: Streams. - Clase 16. Rango de Stream.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import streams.Collector;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        Collector.grouping();
+//        Collector.grouping();
+        IntStream.rangeClosed(1,5).forEach(System.out::println);
     }
 }
