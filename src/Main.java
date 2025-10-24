b4c312894b2be032a7783d54392b52503c066454
Gabriel Hernandez <onedrakodev@gmail.com>
2025-10-23 18:21:19 -0600

    Sección 13: Streams. - Clase 14. Particionado.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import streams.Collector;
 
 public class Main {
     public static void main(String[] args) {
-        Collector.run();
+        Collector.particionado();
     }
 }
--- src/streams/Collector.java
+++ src/streams/Collector.java
@@ -21,6 +21,18 @@ public class Collector {
                 .collect(Collectors.toMap(x-> x.email, x -> x));
 
         System.out.println("resultado = " + resultado);
-        
+    }
+
+    public static void particionado(){
+        List<User> users = List.of(
+                new User(14, "chanchito@holamundo.io"),
+                new User(10, "felipe@holamundo.io"),
+                new User(22, "gabriel@holamundo.io")
+        );
+
+        var particion = users.stream()
+                .collect(Collectors.partitioningBy(x -> x.age > 17));
+
+        System.out.println(particion.get(false));
     }
 }
